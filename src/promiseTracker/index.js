adf9a6128f45d9203be985d70293189cc7cd83c8
iChengbo <chengbo_x@163.com>
2021-04-20 23:33:08 +0800

    ⚡️ 添加参数 allowedInDevMode

--- src/promiseTracker/index.js
+++ src/promiseTracker/index.js
@@ -2,25 +2,30 @@ const noop = () => { };
 
 /**
  * 
- * @param {Function} customHandler 
+ * @param {Function} customHandler
+ * @param {Boolean} allowedInDevMode
  * @returns 
  */
-export const setPromiseUnCatchHandler = (customHandler = noop) => {
-    const type = typeof customHandler;
-    if (type !== 'function') {
-        console.log('setPromiseUnCatchHandler should be callback function, but params is + ', type);
-        console.log('Not setting the Promise handler .. please fix setPromiseUnCatchHandler call');
+export const setPromiseUnCatchHandler = (customHandler = noop, allowedInDevMode = false) => {
+    if (typeof customHandler !== 'function' || typeof allowedInDevMode !== 'boolean') {
+        console.log('setPromiseUnCatchHandler is called with wrong argument types.. first argument should be callback function and second argument is optional should be a boolean');
+        console.log('Not setting the JS handler .. please fix setPromiseUnCatchHandler call');
         return;
     }
-    require('promise/setimmediate/rejection-tracking').enable({
-        allRejections: true,
-        onUnhandled: customHandler,
-        onHandled: (id) => {
-            const warning =
-                `Promise Rejection Handled (id: ${id})\n` +
-                'This means you can ignore any previous messages of the form ' +
-                `"Possible Unhandled Promise Rejection (id: ${id}):"`;
-            console.warn(warning);
-        },
-    });
+    const allowed = allowedInDevMode ? true : !__DEV__;
+    if (allowed) {
+        require('promise/setimmediate/rejection-tracking').enable({
+            allRejections: true,
+            onUnhandled: customHandler,
+            onHandled: (id) => {
+                const warning =
+                    `Promise Rejection Handled (id: ${id})\n` +
+                    'This means you can ignore any previous messages of the form ' +
+                    `"Possible Unhandled Promise Rejection (id: ${id}):"`;
+                console.warn(warning);
+            },
+        });
+    } else {
+        console.log('Skipping setPromiseUnCatchHandler: Reason: In DEV mode and allowedInDevMode = false');
+    }
 };
