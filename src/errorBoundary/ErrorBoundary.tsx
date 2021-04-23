cec075df55064da53f7ae4de025a68e6143d73cb
iChengbo <chengbo_x@163.com>
2021-04-23 22:33:50 +0800

    💡 add comments

--- src/errorBoundary/ErrorBoundary.tsx
+++ src/errorBoundary/ErrorBoundary.tsx
@@ -7,16 +7,30 @@ import { Text } from 'react-native';
 type ComponentDidCatch = ComponentLifecycle<{}, {}>["componentDidCatch"];
 
 export interface ErrorBoundaryProps {
+    /**
+     * Render Component when the ErrorBoundary catches an error.
+     * You can custom ErrorBoundary component.
+     */
     renderBoundary: (error: any) => React.ReactNode | JSX.Element
+    /**
+     * Gets called when the ErrorBoundary catches an error.
+     */
     onDidCatch: NonNullable<ComponentDidCatch>
     children?: React.ReactNode | JSX.Element
 }
 
+/**
+ * Internal ErrorBoundary state.
+ */
 export interface ErrorBoundaryState {
     hasError: boolean
     error: any
 }
 
+/**
+ * ErrorBoundary class
+ * Catches errors using lifecycle methods and renders fallback ui using children or renderBoundary props.
+ */
 export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
     constructor(props: ErrorBoundaryProps) {
         super(props);
--- src/errorBoundary/withErrorBoundary.tsx
+++ src/errorBoundary/withErrorBoundary.tsx
@@ -1,6 +1,12 @@
 import React, { ComponentType } from 'react';
 import { ErrorBoundary, ErrorBoundaryProps } from './ErrorBoundary';
 
+/**
+ * React HOC for creating ErrorBoundaryWrapper component.
+ * @param params.renderBoundary
+ * @param params.onDidCatch
+ * @returns 
+ */
 export const withErrorBoundary = (params: ErrorBoundaryProps) => (WrappedComponent: ComponentType) => {
     const { renderBoundary, onDidCatch } = params;
     return (props: any) => {
